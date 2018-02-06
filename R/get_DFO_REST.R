10d45c4c6dd35ce687341f311039869f73246928
mike <mike.mcmahon@dfo-mpo.gc.ca>
2018-02-06 09:07:02 -0400

    modifies debugging parameter to faciliate use

--- R/get_DFO_REST.R
+++ R/get_DFO_REST.R
@@ -10,8 +10,8 @@
 #' @param save_csv The default value is \code{TRUE}, which means that the
 #' extracted data is saved to a csv file in your working directory.  If
 #' \code{FALSE}, no csv will be created.
-#' @param rec_Start The default is \code{0}.  Primarily for debugging, this
-#' allows the user to select only some of the records, rather than doing a full
+#' @param n_rec The default is \code{0}.  Primarily for debugging, this
+#' allows the user to select only some records, rather than doing a full
 #' extraction.
 #' @family ArcGIS
 #' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
@@ -20,26 +20,37 @@
 #' @importFrom utils txtProgressBar
 #' @importFrom utils write.csv
 #' @export
-get_DFO_REST <-function(service='ADAPT_Canada_Atlantic_Summer_2016', save_csv=TRUE, rec_Start = 0) {
+get_DFO_REST <-function(service='ADAPT_Canada_Atlantic_Summer_2016', save_csv=TRUE, n_rec = 0) {
   timer.start=proc.time() #start timing, for science!
   base_url= paste0('http://geoportal-geoportail.gc.ca/arcgis/rest/services/FGP/',service,'/MapServer/0/')
   extr_Lim = 1000 #can get this many at a time
   
   #total number of records available
   json_cnt_url <- paste0(base_url,'query?where=1=1&f=pjson&returnCountOnly=true')
-  json_cnt <- fromJSON(json_cnt_url)
+  json_cnt <- jsonlite::fromJSON(json_cnt_url)
   if ('error' %in% names(json_cnt)) stop(paste0('\nSomething appears to be wrong with the selected service. Please try again later.
                                                 \nYou can try visiting the following url in your browser to see if there\'s any additional information:
                                                 \n',base_url))
-  rec_N = json_cnt$count
-  print(paste0('Starting extraction of ',rec_N-rec_Start,' records'))
-  
-  pb = txtProgressBar(min=1,
-                      max=ceiling((rec_N-rec_Start)/extr_Lim),
+  rec_Avail = json_cnt$count
+  cat(paste0(rec_Avail,' records available'))
+  if (n_rec==0){
+    wanted = rec_Avail
+    cat('Starting complete extraction')
+  }else{
+    wanted = n_rec
+    cat(paste0('Starting extraction of first ',n_rec,' records'))
+  }
+
+  pb = txtProgressBar(min=0,
+                      max=ceiling(wanted/extr_Lim),
                       style = 3)
-  for (i in 1:ceiling((rec_N-rec_Start)/extr_Lim)){
+  rec_Start=0
+  for (i in 1:ceiling(wanted/extr_Lim)){
+    if ((wanted-rec_Start) < extr_Lim){
+      extr_Lim<-(wanted-rec_Start)
+    }
     this_pull <- paste0(base_url,'query?where=OBJECTID%3E=0&f=pjson&returnCountOnly=false&returnGeometry=false&outFields=*&resultOffset=',rec_Start,'&resultRecordCount=',extr_Lim)
-    this_data <- fromJSON(this_pull)
+    this_data <- jsonlite::fromJSON(this_pull)
     if(i==1){    #first resultset, instantiate df
       this.df=this_data$features$attributes
     }else{       # add to existing df
@@ -54,7 +65,7 @@ get_DFO_REST <-function(service='ADAPT_Canada_Atlantic_Summer_2016', save_csv=TR
   names(this.df) = this_data$fields$alias
   this.df$OBJECTID = NULL
   elapsed=timer.start-proc.time() #determine runtime
-  cat(paste0('\nExtraction completed in ',round(elapsed[3],0)*-1, ' seconds'))
+  cat(paste0('\nExtraction of ',nrow(this.df),' records completed in ',round(elapsed[3],0)*-1, ' seconds'))
   if (save_csv){
     filename = paste0(service,'.csv')
     write.csv(this.df,filename, row.names = FALSE)
